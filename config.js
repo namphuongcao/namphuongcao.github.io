d59f65762943fce0da0f0b5679e8de27f58afcdd
Bui Tho Long <67302157+buitholong123@users.noreply.github.com>
2021-11-03 23:32:59 +0700

    upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    introTitle: 'Heellloo Lam Xinh Gái!',
+    introDesc: `Mình là Nam đêyyyy ♥  `,
+    btnIntro: 'Mạnh Dạn Ấn ĐI Ạ ^^',
+    title: 'Em Sẽ Mãi Ở Bên Anh Nhé !!!!! 😙',
+    desc: ' ♥ ',
+   
+    btnYes: 'Có ♥',
+    btnNo: 'Không ♦',
+    question:'Ý Em Saooooooo',
+    btnReply: 'Send',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: '😘😘',
+    messDesc: 'Chúng ta sẽ cố gắng vì nhau em nhé !!!!.',
+    btnAccept: 'Oki',
+    messLink: 'https://www.facebook.com/phuongnam14082001' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
